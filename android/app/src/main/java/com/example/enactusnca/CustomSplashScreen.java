4eda92e3ef1224bd3bff9cb86fff538de6d130b1
Abdulrahman <abdulrahman.ali19@outlook.com>
2020-11-07 02:40:22 +0200

    - the app icon has been added to both OS's android and ios - a native splash screen has been added to the android build - the app name has been changed to Enactus NC in the android build - colors.xml has been added to the android build - strings.xml has been added to the android build - no more need for category_selector.dart, so it has been deleted

---
+++ android/app/src/main/java/com/example/enactusnca/CustomSplashScreen.java
@@ -0,0 +1,45 @@
+package com.example.enactusnca;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import io.flutter.embedding.android.SplashScreen;
+
+public class CustomSplashScreen implements SplashScreen {
+    private CustomSplashScreenView splashView;
+    @Nullable
+    public View createSplashView(@NonNull Context context, @Nullable Bundle bundle) {
+        if (splashView == null) {
+            splashView = new CustomSplashScreenView(context);
+            splashView.restoreSplashState(bundle);
+        }
+        return splashView;
+    }
+    //closing the splash
+    public void transitionToFlutter(Runnable onTransitionComplete) {
+        if (splashView != null) {
+            splashView.animateAway(onTransitionComplete);
+        }
+        else {
+            onTransitionComplete.run();
+        }
+    }
+    //state saving
+    @Override
+    public boolean doesSplashViewRememberItsTransition() {
+        return true;
+    }
+    @Override
+    @Nullable
+    public Bundle saveSplashScreenState() {
+        if (splashView != null) {
+            return splashView.saveSplashState();
+        } else {
+            return null;
+        }
+    }
+}
