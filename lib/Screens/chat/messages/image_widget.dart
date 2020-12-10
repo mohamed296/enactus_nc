0310e01064e8ada97e05f6639f2f9641852f475e
I7Mustafa <mustafa7ibrahim@gmail.com>
2020-12-10 17:40:18 +0200

    fix send message & clean some code

---
+++ lib/Screens/chat/messages/image_widget.dart
@@ -0,0 +1,115 @@
+import 'package:enactusnca/Helpers/constants.dart';
+import 'package:enactusnca/Models/messages_model.dart';
+import 'package:enactusnca/Screens/Profile/profile.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class ImageWidget extends StatelessWidget {
+  ImageWidget({Key key, this.message}) : super(key: key);
+
+  final MessageModel message;
+  final user = FirebaseAuth.instance.currentUser;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment:
+          user.uid == message.userId ? MainAxisAlignment.end : MainAxisAlignment.start,
+      children: [
+        user.uid == message.userId
+            ? Container(
+                padding: EdgeInsets.all(12.0),
+                height: 64,
+                width: 64,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(34.0),
+                  child: message.userImg != null
+                      ? Image.network(
+                          message.userImg,
+                          fit: BoxFit.contain,
+                          loadingBuilder: (context, child, loadingProgress) {
+                            return loadingProgress == null
+                                ? child
+                                : Center(child: CircularProgressIndicator());
+                          },
+                        )
+                      : Image.asset('assets/images/enactus.png'),
+                ),
+              )
+            : Container(),
+        Container(
+          margin: user.uid == message.userId
+              ? EdgeInsets.only(top: 8.0, bottom: 8.0)
+              : EdgeInsets.only(top: 8.0, bottom: 8.0),
+          padding: EdgeInsets.all(6.0),
+          decoration: BoxDecoration(
+            color: user.uid != message.userId ? Constants.lightBlue : Constants.midBlue,
+            borderRadius: user.uid == message.userId
+                ? BorderRadius.only(
+                    topLeft: Radius.circular(15.0),
+                    bottomLeft: Radius.circular(15.0),
+                  )
+                : BorderRadius.only(
+                    topRight: Radius.circular(15.0),
+                    bottomRight: Radius.circular(15.0),
+                  ),
+          ),
+          width: MediaQuery.of(context).size.width * 0.75,
+          child: Column(
+            mainAxisAlignment:
+                user.uid == message.userId ? MainAxisAlignment.end : MainAxisAlignment.start,
+            children: [
+              ClipRRect(
+                child: Image.network(message.message),
+                borderRadius: BorderRadius.circular(6.0),
+              ),
+              SizedBox(height: 8.0),
+              Text(
+                '${message.userName} - ${message.timestamp.toDate().hour.toString()}:${message.timestamp.toDate().minute.toString()}',
+                style: TextStyle(
+                  fontSize: 12.0,
+                  fontWeight: FontWeight.w200,
+                  color: Colors.grey.shade100,
+                ),
+              ),
+            ],
+          ),
+        ),
+        user.uid != message.userId
+            ? InkWell(
+                onTap: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Profile(
+                        isAppBarEnabled: true,
+                        userId: message.userId,
+                      ),
+                    ),
+                  );
+                },
+                child: Container(
+                  padding: EdgeInsets.all(12.0),
+                  height: 64,
+                  width: 64,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(34.0),
+                    child: message.userImg != null
+                        ? Image.network(
+                            message.userImg,
+                            fit: BoxFit.contain,
+                            loadingBuilder: (context, child, loadingProgress) {
+                              return loadingProgress == null
+                                  ? child
+                                  : Center(child: CircularProgressIndicator());
+                            },
+                          )
+                        : Image.asset('assets/images/enactus.png'),
+                  ),
+                ),
+              )
+            : Container(),
+      ],
+    );
+  }
+}
--- lib/Screens/chat/messages/messages.dart
+++ lib/Screens/chat/messages/messages.dart
@@ -4,8 +4,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:enactusnca/Helpers/constants.dart';
 import 'package:enactusnca/Models/messages_model.dart';
 import 'package:enactusnca/Models/user_model.dart';
-import 'package:enactusnca/Screens/Profile/profile.dart';
 import 'package:enactusnca/Screens/chat/group_member.dart';
+import 'package:enactusnca/Screens/chat/messages/text_widget.dart';
 import 'package:enactusnca/services/database_methods.dart';
 import 'package:enactusnca/services/message_group_services.dart';
 import 'package:enactusnca/services/message_services.dart';
@@ -14,6 +14,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+
+import 'image_widget.dart';
+import 'message_widget.dart';
 
 class Messages extends StatefulWidget {
   static String id = 'messages';
@@ -39,16 +42,15 @@ class Messages extends StatefulWidget {
 }
 
 class _MessagesState extends State<Messages> {
+  final user = FirebaseAuth.instance.currentUser;
   TextEditingController tecMessage = TextEditingController();
   DatabaseMethods databaseMethods = DatabaseMethods();
-  final user = FirebaseAuth.instance.currentUser;
+
   Stream conversationStream;
   DateTime selectedDate = DateTime.now();
 
   File _image;
   String _imgURL;
-  String senimage = '';
-  bool sendim = false;
 
   @override
   void initState() {
@@ -61,15 +63,13 @@ class _MessagesState extends State<Messages> {
   }
 
   Future getImage() async {
-    final pickedFile =
-        await ImagePicker().getImage(source: ImageSource.gallery);
+    final pickedFile = await ImagePicker().getImage(source: ImageSource.gallery);
     if (pickedFile != null) {
       setState(() {
         _imgURL = pickedFile.hashCode.toString();
         _image = File(pickedFile.path);
       });
     }
-
     return _image;
   }
 
@@ -78,15 +78,11 @@ class _MessagesState extends State<Messages> {
       StorageReference ref = FirebaseStorage.instance.ref().child(_imgURL);
       StorageUploadTask storageUploadTask = ref.putFile(_image);
       StorageTaskSnapshot taskSnapshot = await storageUploadTask.onComplete;
-
       String url = await taskSnapshot.ref.getDownloadURL();
-      setState(() {
-        _imgURL = url;
-        senimage = url;
-        sendim = true;
-      });
       return url;
-    } catch (ex) {}
+    } catch (ex) {
+      print(ex.toString());
+    }
   }
 
   Future<void> _selectDate(BuildContext context) async {
@@ -102,11 +98,13 @@ class _MessagesState extends State<Messages> {
     }
   }
 
-  sendMessage(
-      {String type,
-      DateTime dateTime,
-      UserModel userModel,
-      bool sendNotification}) async {
+  sendMessage({
+    String type,
+    DateTime dateTime,
+    UserModel userModel,
+    bool sendNotification,
+    String url,
+  }) async {
     MessageModel messageModel = MessageModel(
       userId: type == 'Task' ? userModel.id : null,
       userImg: userModel?.photoUrl ?? null,
@@ -117,26 +115,21 @@ class _MessagesState extends State<Messages> {
           ? tecMessage.text
           : type == 'Task'
               ? dateTime.toString()
-              : type == 'image'
-                  ? senimage
-                  : null,
+              : url,
     );
     widget.group == true
         ? type == 'Task'
             ? MessageGroupServices()
                 .sendTaskMessage(messageModel, dateTime, sendNotification)
-                .catchError((error) =>
-                    print("getConversationErrors : ${error.toString()}"))
+                .catchError((error) => print("getConversationErrors : ${error.toString()}"))
                 .whenComplete(() => tecMessage.clear())
             : MessageGroupServices()
                 .sendGroupMessage(messageModel)
-                .catchError((error) =>
-                    print("getConversationErrors : ${error.toString()}"))
+                .catchError((error) => print("getConversationErrors : ${error.toString()}"))
                 .whenComplete(() => tecMessage.clear())
         : MessageServices()
             .sendMessage(messageModel)
-            .catchError(
-                (error) => print("getConversationErrors : ${error.toString()}"))
+            .catchError((error) => print("getConversationErrors : ${error.toString()}"))
             .whenComplete(() => tecMessage.clear());
   }
 
@@ -200,16 +193,12 @@ class _MessagesState extends State<Messages> {
                             }).whenComplete(() => Navigator.pop(context)),
                             leading: CircleAvatar(
                               radius: 30,
-                              backgroundImage: snapshot.data[index].photoUrl ==
-                                      null
+                              backgroundImage: snapshot.data[index].photoUrl == null
                                   ? AssetImage("assets/images/person.png")
                                   : NetworkImage(snapshot.data[index].photoUrl),
                             ),
-                            title:
-                                Text(snapshot?.data[index]?.username ?? 'user'),
-                            subtitle: snapshot.data[index].isHead
-                                ? Text('Head')
-                                : Text('Member'),
+                            title: Text(snapshot?.data[index]?.username ?? 'user'),
+                            subtitle: snapshot.data[index].isHead ? Text('Head') : Text('Member'),
                           ),
                         )
                       : CircularProgressIndicator();
@@ -224,172 +213,10 @@ class _MessagesState extends State<Messages> {
 
   _buildMessage(MessageModel message) {
     return message.type == 'Task'
-        ? Container(
-            margin: const EdgeInsets.only(left: 54.0, bottom: 6.0),
-            color: Colors.white,
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    Icon(Icons.list_alt),
-                    SizedBox(width: 12.0),
-                    Text(
-                      'Task assigned to',
-                      style: TextStyle(color: Colors.black),
-                    ),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Icon(Icons.person),
-                    SizedBox(width: 12.0),
-                    Text(
-                      message.userName,
-                      style: TextStyle(color: Colors.black),
-                    ),
-                  ],
-                ),
-                Container(
-                  color: Colors.yellow,
-                  child: Row(
-                    children: [
-                      Icon(
-                        Icons.date_range_rounded,
-                        color: Colors.white,
-                      ),
-                      SizedBox(width: 12.0),
-                      Text(
-                        message.message,
-                        style: TextStyle(color: Colors.black),
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ),
-          )
-        : Row(
-            mainAxisAlignment: user.uid == message.userId
-                ? MainAxisAlignment.end
-                : MainAxisAlignment.start,
-            children: [
-              user.uid == message.userId
-                  ? Container(
-                      padding: EdgeInsets.all(12.0),
-                      height: 64,
-                      width: 64,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(34.0),
-                        child: message.userImg != null
-                            ? Image.network(
-                                message.userImg,
-                                fit: BoxFit.contain,
-                                loadingBuilder:
-                                    (context, child, loadingProgress) {
-                                  return loadingProgress == null
-                                      ? child
-                                      : Center(
-                                          child: CircularProgressIndicator());
-                                },
-                              )
-                            : Image.asset('assets/images/enactus.png'),
-                      ),
-                    )
-                  : Container(),
-              Container(
-                width: MediaQuery.of(context).size.width * 0.75,
-                decoration: BoxDecoration(
-                  color: user.uid != message.userId
-                      ? Constants.lightBlue
-                      : Constants.midBlue,
-                  borderRadius: user.uid == message.userId
-                      ? BorderRadius.only(
-                          topLeft: Radius.circular(15.0),
-                          bottomLeft: Radius.circular(15.0),
-                        )
-                      : BorderRadius.only(
-                          topRight: Radius.circular(15.0),
-                          bottomRight: Radius.circular(15.0),
-                        ),
-                ),
-                margin: user.uid == message.userId
-                    ? EdgeInsets.only(top: 8.0, bottom: 8.0)
-                    : EdgeInsets.only(top: 8.0, bottom: 8.0),
-                padding: EdgeInsets.symmetric(vertical: 25.0, horizontal: 15.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    widget.group == true
-                        ? Text(
-                            message.userName,
-                            style: TextStyle(
-                              fontSize: 16.0,
-                              fontWeight: FontWeight.w400,
-                              color: Colors.blueGrey.shade100,
-                            ),
-                          )
-                        : Container(),
-                    SizedBox(height: 8.0),
-                    (sendim == true)
-                        ? Image.network(senimage)
-                        : Text(
-                            message.message,
-                            style: TextStyle(
-                              fontSize: 16.0,
-                              fontWeight: FontWeight.w600,
-                              color: Colors.blueGrey.shade100,
-                            ),
-                          ),
-                    SizedBox(height: 8.0),
-                    Text(
-                      '${message.timestamp.toDate().hour.toString()}/${message.timestamp.toDate().minute.toString()}/${message.timestamp.toDate().second.toString()}',
-                      style: TextStyle(
-                        fontSize: 12.0,
-                        fontWeight: FontWeight.w200,
-                        color: Colors.grey.shade100,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              user.uid != message.userId
-                  ? InkWell(
-                      onTap: () {
-                        Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => Profile(
-                              isAppBarEnabled: true,
-                              userId: message.userId,
-                            ),
-                          ),
-                        );
-                      },
-                      child: Container(
-                        padding: EdgeInsets.all(12.0),
-                        height: 64,
-                        width: 64,
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(34.0),
-                          child: message.userImg != null
-                              ? Image.network(
-                                  message.userImg,
-                                  fit: BoxFit.contain,
-                                  loadingBuilder:
-                                      (context, child, loadingProgress) {
-                                    return loadingProgress == null
-                                        ? child
-                                        : Center(
-                                            child: CircularProgressIndicator());
-                                  },
-                                )
-                              : Image.asset('assets/images/enactus.png'),
-                        ),
-                      ),
-                    )
-                  : Container(),
-            ],
-          );
+        ? TaskWidget(messageModel: message)
+        : message.type == 'Message'
+            ? MessageWidget(message: message, group: widget.group)
+            : ImageWidget(message: message);
   }
 
   _buildMessageComposer() {
@@ -411,17 +238,18 @@ class _MessagesState extends State<Messages> {
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: <Widget>[
                         IconButton(
-                            icon: Icon(
-                              Icons.image,
-                              color: Colors.yellow,
-                            ),
-                            onPressed: () async {
-                              getImage();
-
-                              await uploadImage(context).then(
-                                (url) => sendMessage(type: 'image'),
-                              );
-                            }),
+                          icon: Icon(
+                            Icons.image,
+                            color: Colors.yellow,
+                          ),
+                          onPressed: () async {
+                            await getImage().then(
+                              (value) => uploadImage(context).then(
+                                (url) => sendMessage(type: 'image', url: url),
+                              ),
+                            );
+                          },
+                        ),
                         Expanded(
                           child: TextField(
                             controller: tecMessage,
@@ -438,9 +266,7 @@ class _MessagesState extends State<Messages> {
                             ),
                           ),
                         ),
-                        snapshot.data.isHead ||
-                                snapshot.data.isAdmin &&
-                                    widget.groupName != null
+                        snapshot.data.isHead || snapshot.data.isAdmin && widget.groupName != null
                             ? IconButton(
                                 icon: Icon(Icons.table_chart),
                                 color: Constants.yellow,
@@ -500,8 +326,7 @@ class _MessagesState extends State<Messages> {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) =>
-                            GroupMember(groupName: widget.groupName),
+                        builder: (context) => GroupMember(groupName: widget.groupName),
                       ),
                     );
                   },
